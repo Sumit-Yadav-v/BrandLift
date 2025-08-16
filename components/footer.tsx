5ead6ea914e40cfc388fdf86a3d8d2a22979b323
Sumit-Yadav-v <sumit.yydv@gmail.com>
2025-08-16 21:14:26 +0530

    Update footer.tsx
    coment icon

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,14 +14,14 @@ export function Footer() {
               We lift your brand to new heights with professional websites that convert visitors into customers. No
               commissions, full ownership, transparent pricing.
             </p>
-           /* 
+            { /* 
             <div className="flex space-x-4">
               <Facebook className="w-6 h-6 hover:text-primary transition-colors cursor-pointer hover:scale-110 transform duration-200" />
               <Twitter className="w-6 h-6 hover:text-primary transition-colors cursor-pointer hover:scale-110 transform duration-200" />
               <Instagram className="w-6 h-6 hover:text-primary transition-colors cursor-pointer hover:scale-110 transform duration-200" />
               <Linkedin className="w-6 h-6 hover:text-primary transition-colors cursor-pointer hover:scale-110 transform duration-200" />
             </div>
-            */
+            */}
           </div>
 
           <div>
