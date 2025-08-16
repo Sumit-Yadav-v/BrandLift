060f6e024a2d8e0e2197de4e7125dc6a5955aa02
pratikjain1510 <prxtikjxin@gmail.com>
2025-08-17 00:08:55 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -54,7 +54,7 @@ export function Footer() {
         </div>
 
         <div className="border-t border-background/20 mt-12 pt-8 text-center text-background/60">
-          <p>&copy; 2025 BrandLift. All rights reserved.</p>
+          <p>&copy; 2025 Brand All rights reserved.</p>
         </div>
       </div>
     </footer>
